0f840ff6150a3e244cc854f80ef9f43773a2ed28
sohee120 <yuhsohee120@gmail.com>
2022-04-01 23:00:34 +0900

    add: email certification

--- sequence/sequence/CertificationViewController.swift
+++ sequence/sequence/CertificationViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import KakaoSDKAuth
+import Alamofire
 
 class CertificationViewController: UIViewController {
 
@@ -22,7 +24,31 @@ class CertificationViewController: UIViewController {
     
     @IBAction func onBtnResend(_ sender: UIButton) {
     }
-
+    
+    
+    @IBAction func onBtnComplete(_ sender: UIButton) {
+        
+        let token = ["token":self.certificationNumTextField.text!]
+        
+        let url = "http://localhost:8879/auth/valid"
+        let dataRequest = AF.request(url, method: .post, parameters: token, encoding: JSONEncoding.default)
+        // 서버 응답 처리
+        dataRequest.validate().responseData { response in
+                switch response.result {
+                        case .success:
+                            let complete = UIAlertController(title: "", message: "가입이 완료되었습니다", preferredStyle: .alert)
+                            self.present(complete, animated: true)
+                    
+                        case let .failure(error):
+                            let fail = UIAlertController(title: "", message: "인증번호가 올바르지 않습니다.", preferredStyle: .alert)
+                            self.present(fail, animated: true)
+                                    print(error)
+                            }
+                print(response.result)
+                }
+        
+    }
+    
     
     
     
