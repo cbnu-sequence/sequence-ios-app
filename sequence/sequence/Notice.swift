5820f2aaa9176edd9e1f76486a4a6b6cf09748d0
sohee120 <yuhsohee120@gmail.com>
2022-05-19 23:38:29 +0900

    Add: Show notice data on notice cell

--- sequence/sequence/Notice.swift
+++ sequence/sequence/Notice.swift
@@ -8,22 +8,36 @@
 import Foundation
 
 
-//struct Notice: Codable {
-//
-//    var _id: String
-//    var title: String
-//    var category1: String
-//    var category2: String?
-//    var content: String
-//    var writer: Any
-//    var files: Array<String>?
-//    var images: Array<String>?
-//    var createdAt: Date
-//    var updatedAt: Date
-//    var __v: Int
-//
-//}
 
-//static var all : [Notice] = Notice.
+struct Writer : Codable {
+    var _id: String?
+    var name: String?
+}
+
+struct Notice : Codable {
+
+    var _id: String?
+    var title: String?
+    var category1: String?
+    var category2: String?
+    var content: String?
+    var writer: Writer?
+    var files: Array<String>?
+    var images: Array<String>?
+    var createdAt: String?
+    var updatedAt: String?
+    var __v: Int?
+
+}
+
+struct APIResponse: Codable {
+    let data: [Notice]
+    var status: Int
+    var message: String
+    var success: String
+    var count: Int
+}
+
+
 
 
--- sequence/sequence/NoticeViewController.swift
+++ sequence/sequence/NoticeViewController.swift
@@ -11,12 +11,12 @@ import Alamofire
 import KakaoSDKCommon
 import KakaoSDKUser
 
-class NoticeViewController: UIViewController {
+class NoticeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    
+
     
     public var page = 1
-   // public let notices = [Notice]()
+    var noticeData: [Notice] = []
     
     
     @IBOutlet weak var noticeTableView: UITableView!
@@ -24,40 +24,78 @@ class NoticeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // 테이블 뷰의 일반 이벤트 처리
+        noticeTableView.delegate = self
+        // 테이블 뷰의 데이터소스 처리
+        noticeTableView.dataSource = self
+        
+        getNoticeData()
+        
 
      
     }
     
-//    func getNoticeData() {
-//
-//      // var page = 1
-//       let hashPage = ["page":page]
-//
-//   let url = "http://localhost:8879/post/notice"
-//   let dataRequest = AF.request(url, method: .get, parameters: hashPage, encoding: JSONEncoding.default)
-//   // 서버 응답 처리
-//   dataRequest.validate().responseData { response in
-//           switch response.result {
-//                   case .success:
-//               notices.append(response.data)
-//                   case let .failure(error):
-//                               print("error")
-//                       }
-//           print("get요청 결과\(response)")
-//           }
-//   }
-}
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        getNoticeData()
+    }
     
-    
-    
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//
-//
-//        return cell
-//    }
-//
-    
+    func getNoticeData() {
 
+       var page = 1
+//       let hashPage = ["page": page]
+         
+
+   
+   let url = "http://localhost:8879/post/notice?page=\(page)"
+   let header : HTTPHeaders = ["Content-Type": "application/json"]
+   let dataRequest = AF.request(url, method: .get, encoding: JSONEncoding.default, headers: header)
+   // 서버 응답 처리
+        dataRequest.validate().responseJSON { (response) in
+           switch response.result {
+                   case .success(let obj):
+               do {
+                   let dataJSON = try JSONSerialization.data(withJSONObject:obj, options: .prettyPrinted)
+                   let getData = try JSONDecoder().decode(APIResponse.self, from: dataJSON)
+                   self.noticeData = getData.data
+                   DispatchQueue.main.async {
+                       self.noticeTableView.reloadData()
+                   }
+                   
+               } catch(let error) {
+                   print(error)
+               }
+           case .failure(let error) :
+               print(error)
+           }
+
+           }
+   }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.noticeData.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        let cell = self.noticeTableView.dequeueReusableCell(withIdentifier: "NoticeCell", for: indexPath) as! NoticeTableViewCell
+        
+        let notice: Notice = self.noticeData[indexPath.row]
+        cell.dateLabel?.text = notice.createdAt
+        cell.titleLabel.text = notice.title
+
+
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 65
+    }
+    
+}
 
 
 
