3a5da9cdb45a0e8644d1ba3a1bf154f180d0f2f7
sohee120 <yuhsohee120@gmail.com>
2022-05-30 23:00:41 +0900

    Fix : sort notices

--- sequence/sequence/NoticeViewController.swift
+++ sequence/sequence/NoticeViewController.swift
@@ -46,7 +46,7 @@ class NoticeViewController: UIViewController, UITableViewDelegate, UITableViewDa
     func getNoticeData() {
 
    
-   let url = "http://localhost:8879/post/notice?page=\(page)"
+   let url = "http://localhost:8879/post/notice?sort=-createdAt&page=\(page)&limit=20"
    let header : HTTPHeaders = ["Content-Type": "application/json"]
    let dataRequest = AF.request(url, method: .get, encoding: JSONEncoding.default, headers: header)
    // 서버 응답 처리
