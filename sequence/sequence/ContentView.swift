fd349a43d70f32b816f19376ec8d9d810fa8b9ff
Hyun0l <101308526+Hyun0l@users.noreply.github.com>
2022-06-01 15:09:06 +0900

    Add: 정보공유 프로젝트 웹뷰 추가

---
+++ sequence/sequence/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  sequence
+//
+//  Created by 이현영 on 2022/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
